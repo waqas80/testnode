fc313c6c756166994038524b22aec1f8fb9f4b78
Waqas <waqas80@msn.com>
2022-09-10 09:45:20 +0400

    Update cardservice.js

--- cardservice.js
+++ cardservice.js
@@ -5,7 +5,7 @@ const request = require('postman-request');
 const options = {
     url: 'https://api.clashroyale.com/v1/cards',
     headers: {
-      'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImVkNmY3YzAxLTY2MWItNDhhNi05ZDBkLTA1YzI2YTMyYjFiNCIsImlhdCI6MTY2MTk2MDQ5OCwic3ViIjoiZGV2ZWxvcGVyLzk0MjVhOTBjLWI4NmYtM2RkOS03YmYzLWU4ZWE3ZjFjNGY2MiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI1Mi44Ny45MS44OSJdLCJ0eXBlIjoiY2xpZW50In1dfQ.Keayn-g0cYWLjyWysBVNuy1WtJm7X5FHgYzJUHJpfO121qeolSwY6eI4N-SBnWP1GxYsJKWmUOJejOT9JFJrYA'
+      'Authorization': 'Bearer xxxxxxx'
     }
   };
   
